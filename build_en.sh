7886ea305db64f237ba98c067f54dbc74a81c842
Terry Tao <librakevin@gmail.com>
2015-03-23 21:36:47 +0800

    Minor update to csv and build scripts

--- build_en.sh
+++ build_en.sh
@@ -3,6 +3,6 @@
 DIR=$(dirname $0)
 
 . ${DIR}/common.sh
-
+prebuild
 cd ${TARGET_DIR}
 pdflatex ../resume_en.tex
--- build_zh.sh
+++ build_zh.sh
@@ -3,6 +3,7 @@
 DIR=$(dirname $0)
 
 . ${DIR}/common.sh
+prebuild
 
 cd ${TARGET_DIR}
 xelatex ../resume_zh.tex
